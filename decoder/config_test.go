8c0724a9ee20eec8c4c2329079ae208b1007893c
David Iunevich <f3oall@gmail.com>
2024-02-15 11:17:02 -0500

    test: added tests for scoped decoding

--- decoder/config_test.go
+++ decoder/config_test.go
@@ -71,3 +71,198 @@ func TestScope_DB(t *testing.T) {
 		AdminWrite:       true,
 	}, s)
 }
+
+var adminDecoder = &Config{}
+
+func TestScope_Admin(t *testing.T) {
+	data := newScopedStructJSON()
+	var s testdata.ScopedStruct
+	err := adminDecoder.Unmarshal(data, &s)
+	require.NoError(t, err)
+	require.Equal(t, testdata.ScopedStruct{
+		Name:             "test",
+		NoDB:             true,
+		Read:             false,
+		ReadCreate:       true,
+		ReadUpdate:       true,
+		Update:           true,
+		Create:           true,
+		Write:            true,
+		NoClient:         true,
+		ClientRead:       true,
+		ClientReadCreate: true,
+		ClientReadUpdate: true,
+		ClientUpdate:     true,
+		ClientCreate:     true,
+		ClientWrite:      true,
+		NoAdmin:          false,
+		AdminRead:        false,
+		AdminReadCreate:  true,
+		AdminReadUpdate:  true,
+		AdminUpdate:      true,
+		AdminCreate:      true,
+		AdminWrite:       true,
+	}, s)
+}
+
+func TestScope_Admin_Create(t *testing.T) {
+	data := newScopedStructJSON()
+	var s testdata.ScopedStruct
+	err := adminDecoder.UnmarshalScoped(data, &s, scopes.DECODE_CREATE)
+	require.NoError(t, err)
+	require.Equal(t, testdata.ScopedStruct{
+		Name:             "test",
+		NoDB:             true,
+		Read:             false,
+		ReadCreate:       true,
+		ReadUpdate:       false,
+		Update:           false,
+		Create:           true,
+		Write:            true,
+		NoClient:         true,
+		ClientRead:       true,
+		ClientReadCreate: true,
+		ClientReadUpdate: true,
+		ClientUpdate:     true,
+		ClientCreate:     true,
+		ClientWrite:      true,
+		NoAdmin:          false,
+		AdminRead:        false,
+		AdminReadCreate:  true,
+		AdminReadUpdate:  false,
+		AdminUpdate:      false,
+		AdminCreate:      true,
+		AdminWrite:       true,
+	}, s)
+
+}
+
+func TestScope_Admin_Update(t *testing.T) {
+	data := newScopedStructJSON()
+	var s testdata.ScopedStruct
+	err := adminDecoder.UnmarshalScoped(data, &s, scopes.DECODE_UPDATE)
+	require.NoError(t, err)
+	require.Equal(t, testdata.ScopedStruct{
+		Name:             "test",
+		NoDB:             true,
+		Read:             false,
+		ReadCreate:       false,
+		ReadUpdate:       true,
+		Update:           true,
+		Create:           false,
+		Write:            true,
+		NoClient:         true,
+		ClientRead:       true,
+		ClientReadCreate: true,
+		ClientReadUpdate: true,
+		ClientUpdate:     true,
+		ClientCreate:     true,
+		ClientWrite:      true,
+		NoAdmin:          false,
+		AdminRead:        false,
+		AdminReadCreate:  false,
+		AdminReadUpdate:  true,
+		AdminUpdate:      true,
+		AdminCreate:      false,
+		AdminWrite:       true,
+	}, s)
+
+}
+
+var clientDecoder = &Config{
+	ContextScope: scopes.CONTEXT_CLIENT,
+}
+
+func TestScope_Client(t *testing.T) {
+	data := newScopedStructJSON()
+	var s testdata.ScopedStruct
+	err := clientDecoder.Unmarshal(data, &s)
+	require.NoError(t, err)
+	require.Equal(t, testdata.ScopedStruct{
+		Name:             "test",
+		NoDB:             true,
+		Read:             false,
+		ReadCreate:       true,
+		ReadUpdate:       true,
+		Update:           true,
+		Create:           true,
+		Write:            true,
+		NoClient:         false,
+		ClientRead:       false,
+		ClientReadCreate: true,
+		ClientReadUpdate: true,
+		ClientUpdate:     true,
+		ClientCreate:     true,
+		ClientWrite:      true,
+		NoAdmin:          true,
+		AdminRead:        true,
+		AdminReadCreate:  true,
+		AdminReadUpdate:  true,
+		AdminUpdate:      true,
+		AdminCreate:      true,
+		AdminWrite:       true,
+	}, s)
+}
+
+func TestScope_Client_Create(t *testing.T) {
+	data := newScopedStructJSON()
+	var s testdata.ScopedStruct
+	err := clientDecoder.UnmarshalScoped(data, &s, scopes.DECODE_CREATE)
+	require.NoError(t, err)
+	require.Equal(t, testdata.ScopedStruct{
+		Name:             "test",
+		NoDB:             true,
+		Read:             false,
+		ReadCreate:       true,
+		ReadUpdate:       false,
+		Update:           false,
+		Create:           true,
+		Write:            true,
+		NoClient:         false,
+		ClientRead:       false,
+		ClientReadCreate: true,
+		ClientReadUpdate: false,
+		ClientUpdate:     false,
+		ClientCreate:     true,
+		ClientWrite:      true,
+		NoAdmin:          true,
+		AdminRead:        true,
+		AdminReadCreate:  true,
+		AdminReadUpdate:  true,
+		AdminUpdate:      true,
+		AdminCreate:      true,
+		AdminWrite:       true,
+	}, s)
+
+}
+
+func TestScope_Client_Update(t *testing.T) {
+	data := newScopedStructJSON()
+	var s testdata.ScopedStruct
+	err := clientDecoder.UnmarshalScoped(data, &s, scopes.DECODE_UPDATE)
+	require.NoError(t, err)
+	require.Equal(t, testdata.ScopedStruct{
+		Name:             "test",
+		NoDB:             true,
+		Read:             false,
+		ReadCreate:       false,
+		ReadUpdate:       true,
+		Update:           true,
+		Create:           false,
+		Write:            true,
+		NoClient:         false,
+		ClientRead:       false,
+		ClientReadCreate: false,
+		ClientReadUpdate: true,
+		ClientUpdate:     true,
+		ClientCreate:     false,
+		ClientWrite:      true,
+		NoAdmin:          true,
+		AdminRead:        true,
+		AdminReadCreate:  true,
+		AdminReadUpdate:  true,
+		AdminUpdate:      true,
+		AdminCreate:      true,
+		AdminWrite:       true,
+	}, s)
+}
